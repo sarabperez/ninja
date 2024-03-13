2778baccae0bb47dcdbfa29072e44c8615aa7840
Sara Perez <sarabelenpz@gmail.com>
2024-03-13 15:28:11 -0300

    :[TAREA]sensei

--- script.js
+++ script.js
@@ -15,8 +15,35 @@ class Ninja{
         this.salud +=10;
     }
 }
+
 const ninja1= new Ninja('akari', 5);
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
+
+class Sensei extends Ninja{
+    constructor(
+        nombre,
+        salud=200,
+        fuerza=10,
+        velocidad=10,
+        sabiduria=10
+        ) {
+        super(nombre,salud);
+        this.fuerza=fuerza;
+        this.velocidad=velocidad;
+        this.sabiduria=sabiduria;
+        
+    }
+    speakWisdom(){
+        console.log("La sabiduría del sensei dice: El camino hacia la victoria no es fácil pero es inevitable")
+        super.drinkSake();
+    }
+
+}
+
+const sensei1= new Sensei('Kyotaro', 200);
+console.log(sensei1);
+sensei1.speakWisdom();
+console.log(sensei1);
